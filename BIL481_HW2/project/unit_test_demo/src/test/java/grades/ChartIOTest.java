df233247861e1a0d251c848399057773b08d15fb
nurselisar <nurselisarr@gmail.com>
2020-03-17 22:20:52 +0300

    File paths are added,exceptions are handled.

--- BIL481_HW2/project/unit_test_demo/src/test/java/grades/ChartIOTest.java
+++ BIL481_HW2/project/unit_test_demo/src/test/java/grades/ChartIOTest.java
@@ -7,15 +7,27 @@ import org.junit.Assert.*;
 public class ChartIOTest {
 	
 	ChartIO io = new ChartIO();
+	
 	@Test
 	public void test_readFile_happyPath() {
+		String path = "src/test/resources/grades_normal.csv";
 		
+		int[] dizi = new int[3];
+		dizi[0] = 15;
+		dizi[1] = 10;
+		dizi[2] = 0;
+		
+		Assert.assertArrayEquals(dizi,io.readFile(path));
 	}
 	
-	@Test 
+	@Test( expected = IllegalArgumentException.class)
 	public void test_readFile_SadPath() {
-		
+		io.readFile("nurseli");
 	}
 	
-	
+	@Test (expected = NumberFormatException.class)
+	public void test_ExceptionFileReading() {
+		String path = "src/test/resources/grades_invalid.csv";
+		io.readFile(path);
+	}
 }
